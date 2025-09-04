267f49e50e68278981abbf5c9d1aff5e151d469d
ziqi_ <ziqi_xv@163.com>
2025-09-04 23:39:44 +0800

    init

--- Algorithms/src/com/huawei/it/v1_sort/Sort.java
+++ Algorithms/src/com/huawei/it/v1_sort/Sort.java
@@ -1,28 +1,34 @@
 package com.huawei.it.v1_sort;
 
+import java.util.Arrays;
+
 public class Sort {
     public static void main(String[] args) {
-        int[] arr = {1, 3}; // Java声明数组
+        int[] arr = {1, 3, 5}; // Java声明数组
         quickSort(arr, 0, 2);
-        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void quickSort(int[] arr, int left, int right) {
-        if (arr == null || arr.length == 0) {
+    public static void quickSort(int[] a, int left, int right) {
+        if (a == null || a.length == 0) {
+            return;
+        }
+        if (left > right) { // 加上判断
             return;
         }
         int i = left;
         int j = right;
-        for (; i < j; ) {
-            for (; i <= j && arr[j] < arr[left]; j--) ;
-            for (; i <= j && arr[i] > arr[right]; i++) ;
+        while (i < j) {
+            for (; i < j && a[j] >= a[left]; j--) ; // i<j   >=
+            for (; i < j && a[i] <= a[right]; i++) ;
             if (i >= j) {
                 continue;
             }
-            swap(arr, i, j);
+            swap(a, i, j);
         }
-        quickSort(arr, left, i - 1);
-        quickSort(arr, i + 1, right);
+        swap(a, left, i); // 漏了, 交换锚点元素和碰撞元素
+        quickSort(a, left, i - 1);
+        quickSort(a, i + 1, right);
     }
 
     public static void swap(int[] arr, int index1, int index2) {
